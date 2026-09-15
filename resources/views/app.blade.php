c4ed5c923ad6c054bebf57855eb17c810cd2cfdb
hanzputram <codmm7083@gmail.com>
2026-09-15 13:35:22 +0700

    ok..

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1821,7 +1821,7 @@
             <div class="hero-cta-group">
               <a href="{{ route('products.index') }}" class="btn-cta-white" id="btnProductList">
                 <span>
-                  <span class="ats-lang-en" data-i18n="hero.btn_products">Explore Product Catalog</span>
+                  <span class="ats-lang-en" data-i18n="hero.btn_products">Product Catalog</span>
                   <span class="ats-lang-id" data-i18n="hero.btn_products">Lihat Katalog Produk</span>
                 </span>
                 <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round">
